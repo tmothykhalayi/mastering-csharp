f605fed6261b57f265fb5979d6a5780acd7c4188
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-11-02 16:30:54 +0300

    Add initial project structure for MyFileApp and FirstCSharp
    - Created MyFileApp project with necessary files and configurations.
    - Added project files including .csproj, .dll, .pdb, and runtime configurations.
    - Generated build artifacts and cache files for MyFileApp.
    - Established FirstCSharp project with basic structure and configurations.
    - Included .sln file, .csproj, and initial source files for FirstCSharp.
    - Removed obsolete files from FirstCSharp project to clean up the repository.

--- ASP.NET/002/obj/Debug/net9.0/FirstCSharp.AssemblyInfo.cs
+++ ASP.NET/002/obj/Debug/net9.0/FirstCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3238b064f138b672a7c8c04506529edbdf6b897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+778b1e071aa566cacc30704849da813330295085")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
